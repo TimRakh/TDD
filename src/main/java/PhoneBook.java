9372e09d167707f84abfa4d257cac52baf748c1a
Tim Rakh <nbveh0914@gmail.com>
2023-05-03 19:56:20 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,14 @@
-public class Phonebook {
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PhoneBook {
+
+    Map<String, String> phone = new TreeMap<>();
+
+    public int add(String name, String number) {
+        if (!phone.containsKey(name)) {
+            phone.put(name, number);
+        }
+        return phone.size();
+    }
 }
--- src/test/java/PhonebookTest.java
+++ src/test/java/PhonebookTest.java
@@ -1,2 +1,41 @@
-public class PhonebookTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneBookTest {
+    PhoneBook phoneBook;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Начало теста");
+        phoneBook = new PhoneBook();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Окончание теста");
+        phoneBook = null;
+    }
+
+    public static Stream<Arguments> sourceAdd() {
+        return Stream.of(
+                Arguments.of(2, "Ivan", "461379", "Olya", "461379"),
+                Arguments.of(1, "Ivan", "461379", "Ivan", "461379")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceAdd")
+    void add_test(int expected, String name1, String number1, String name2, String number2) {
+        phoneBook.add(name1, name2);
+        Assertions.assertEquals(expected, phoneBook.add(name2, number2));
+    }
 }
